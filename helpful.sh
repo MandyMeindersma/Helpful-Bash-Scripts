bbc830449cd9bf7a90d4f24fe115fc1daf87c986
Mandy Meindersma <meinders@ualberta.ca>
2023-04-19 19:40:19 -0700

    adding some bit bucket pr opening things

--- helpful.sh
+++ helpful.sh
@@ -6,6 +6,20 @@ function newpr () {
     esac
 }
 
+
+# see pr's on a bit bucket repo
+function pr() {
+  open "https://bitbucket.prod.aws.<place>.com/projects/<name>/repos/$1/pull-requests"
+}
+# Open a new PR on bitbucket
+function new_bb_pr() {
+  sourceBranch=$(cd "$HOME/dev/$1" && git symbolic-ref HEAD)
+  amp="&"
+  open "https://bitbucket.prod.aws.<place>.com/projects/<name>/repos/$1/pull-requests?create${amp}targetBranch=refs/heads/master${amp}sourceBranch=$sourceBranch"
+}
+
+
+
 function editZsh () {
     code ~/.zshrc
 }
